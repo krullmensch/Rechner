d2065b0809e5c0e695a560613e414a2819ebd6ab
krullmensch <marvin.krullmann@gmail.com>
2016-01-03 20:34:42 +0100

    Rechner

---
+++ rechnertest.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+x = int(input("Gebe erste Variable ein: ")) 
+
+y = int(input("Gebe zweite Variable ein: ")) 
+
+z = x + y 
+
+if z < 10: 
+	print("Das Ergebnis ist kleiner als Zehn")
+	
+if z > 10:
+	print("Das Ergebnis ist groesser als Zehn")
+	
+if z == 10:
+	print("Das Ergebnis ist Zehn")
+
+int(input())
